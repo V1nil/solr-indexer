08beb71fd6e0829f03b28bb9cf9eee0503fe06cf
wordpress <wordpress@wordpress>
2018-01-15 18:10:00 +0100

    Subida inicial del plugin y cabeceras

---
+++ solr-indexer.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Solr Indexer
+Description: Test
+Author: Samuel García Alfageme - Mindala Technologies
+Version: 0.0.1
+Author URI: http://www,mindalatech.com
+*/
+
+defined( 'ABSPATH' ) or die( 'bitch please... GTFO' );
+
